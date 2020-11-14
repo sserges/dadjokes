268375883193f9d8d63cb21c30b3315914c8532f
sserges <agounkeserge@gmail.com>
2020-11-14 19:08:03 +0000

    Jokes page added

---
+++ components/Joke.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="joke">
+    <p>{{ joke }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Joke',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['joke', 'id']
+}
+</script>
+
+<style>
+  .joke {
+    padding: 1rem;
+    border: 1px dotted #ccc;
+    margin: 1rem 0;
+  }
+</style>
