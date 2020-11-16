b05a4d9915847dac07241b513865dbfcafc7726b
sserges <agounkeserge@gmail.com>
2020-11-16 11:04:00 +0000

    Joke Search component added

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes @search-text="searchText" />
     <Joke v-for="joke in jokes" :id="joke.id" :key="joke.id" :joke="joke.joke" />
   </div>
 </template>
@@ -8,11 +9,13 @@
 import axios from 'axios'
 
 import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
 
 export default {
 
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data () {
     return {
@@ -32,6 +35,23 @@ export default {
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error)
+    }
+  },
+  methods: {
+    async searchText (text) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config)
+        this.jokes = res.data.results
+      } catch (error) {
+      // eslint-disable-next-line no-console
+        console.error(error)
+      }
     }
   },
   head () {
